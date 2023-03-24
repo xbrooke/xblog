5e05e5e4dfa417de991c589a9cac6692e9265460
xbrooke <107459849+xbrooke@users.noreply.github.com>
2023-03-24 09:33:12 +0000

    Deploying to gh-pages from  @ 8be4112d9b6e86ca7c6b4e2e886823d4f0290498 🚀

---
+++ assets/brooke_blog_haohaozhu_ggsk.md.60988351.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as t}from"./app.a140e800.js";const h=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"brooke/blog/haohaozhu/ggsk.md"}'),a={name:"brooke/blog/haohaozhu/ggsk.md"};function r(s,c,n,p,_,d){return t(),o("div")}const i=e(a,[["render",r]]);export{h as __pageData,i as default};
---
+++ assets/brooke_blog_haohaozhu_yufa.md.cd836c4b.js
@@ -0,0 +1,66 @@
+import{_ as s,c as a,o as n,e as l}from"./app.a140e800.js";const h=JSON.parse('{"title":"Markdown语法","description":"","frontmatter":{},"headers":[{"level":2,"title":"高亮","slug":"高亮","link":"#高亮","children":[]},{"level":2,"title":"源码表格","slug":"源码表格","link":"#源码表格","children":[]},{"level":2,"title":"文线框","slug":"文线框","link":"#文线框","children":[]},{"level":2,"title":"表头","slug":"表头","link":"#表头","children":[]},{"level":2,"title":"Markdown 图片语法","slug":"markdown-图片语法","link":"#markdown-图片语法","children":[]},{"level":2,"title":"超链接","slug":"超链接","link":"#超链接","children":[]},{"level":2,"title":"标题区域（对vitepress没用）","slug":"标题区域-对vitepress没用","link":"#标题区域-对vitepress没用","children":[]}],"relativePath":"brooke/blog/haohaozhu/yufa.md"}'),t={name:"brooke/blog/haohaozhu/yufa.md"},e=l(`<h1 id="markdown语法" tabindex="-1">Markdown语法 <a class="header-anchor" href="#markdown语法" aria-hidden="true">#</a></h1><h2 id="高亮" tabindex="-1">高亮 <a class="header-anchor" href="#高亮" aria-hidden="true">#</a></h2><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight" tabindex="0"><code><span class="line"><span style="color:#A6ACCD;">\`SN735\`</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div><h2 id="源码表格" tabindex="-1">源码表格 <a class="header-anchor" href="#源码表格" aria-hidden="true">#</a></h2><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight" tabindex="0"><code><span class="line"><span style="color:#A6ACCD;">&lt;table&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">   &lt;tr&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;th&gt;CPU&lt;/th&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;th&gt;内存&lt;/th&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;th&gt;硬盘&lt;/th&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;th&gt;价格&lt;/th&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;th&gt;购买链接&lt;/th&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">   &lt;/tr&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">   &lt;tr&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;td rowspan=&quot;2&quot;&gt;N5105&lt;/td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;td&gt;8GB&lt;/td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;td&gt;不带/128G&lt;/td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;td&gt;￥1069/￥1269&lt;/td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">         &lt;a href=&quot;https://item.taobao.com/item.htm?ft=t&amp;id=682025492099&quot; target=&quot;_blank&quot;&gt;购买&lt;/a&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;/td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">   &lt;/tr&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">   &lt;tr&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;td&gt;16GB&lt;/td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;td&gt;不带/512G&lt;/td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;td&gt;￥1369/￥1769&lt;/td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">         &lt;a href=&quot;https://item.taobao.com/item.htm?ft=t&amp;id=682025492099&quot; target=&quot;_blank&quot;&gt;购买&lt;/a&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;/td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">   &lt;/tr&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">   &lt;tr&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;td rowspan=&quot;2&quot;&gt;N6005&lt;/td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;td&gt;8GB&lt;/td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;td&gt;不带/128G&lt;/td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;td&gt;￥1269/￥1469&lt;/td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">         &lt;a href=&quot;https://item.taobao.com/item.htm?ft=t&amp;id=682025492099&quot; target=&quot;_blank&quot;&gt;购买&lt;/a&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;/td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">   &lt;/tr&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">   &lt;tr&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;td&gt;16GB&lt;/td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;td&gt;不带/512G&lt;/td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;td&gt;￥1469/￥1969&lt;/td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">         &lt;a href=&quot;https://item.taobao.com/item.htm?ft=t&amp;id=682025492099&quot; target=&quot;_blank&quot;&gt;购买&lt;/a&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;/td&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">   &lt;/tr&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;/table&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div><h2 id="文线框" tabindex="-1">文线框 <a class="header-anchor" href="#文线框" aria-hidden="true">#</a></h2><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight" tabindex="0"><code><span class="line"><span style="color:#A6ACCD;">:::tip 提示 蓝色块</span></span>
+<span class="line"><span style="color:#A6ACCD;">请输入内容</span></span>
+<span class="line"><span style="color:#A6ACCD;">:::</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight" tabindex="0"><code><span class="line"><span style="color:#A6ACCD;">::: warning 黄色块</span></span>
+<span class="line"><span style="color:#A6ACCD;">请输入内容</span></span>
+<span class="line"><span style="color:#A6ACCD;">:::</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div><h2 id="表头" tabindex="-1">表头 <a class="header-anchor" href="#表头" aria-hidden="true">#</a></h2><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight" tabindex="0"><code><span class="line"><span style="color:#A6ACCD;">---</span></span>
+<span class="line"><span style="color:#A6ACCD;">title: 配置及价格</span></span>
+<span class="line"><span style="color:#A6ACCD;">---</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight" tabindex="0"><code><span class="line"><span style="color:#A6ACCD;">---</span></span>
+<span class="line"><span style="color:#A6ACCD;">title: 默认系统</span></span>
+<span class="line"><span style="color:#A6ACCD;">description: 默认预装的系统，仅限带硬盘设备。</span></span>
+<span class="line"><span style="color:#A6ACCD;">---</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div><h2 id="markdown-图片语法" tabindex="-1">Markdown 图片语法 <a class="header-anchor" href="#markdown-图片语法" aria-hidden="true">#</a></h2><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight" tabindex="0"><code><span class="line"><span style="color:#A6ACCD;">![](https://github.com/KoolCore/wiki.ikoolcore.com/blob/main/pic/R1-banner-2023.jpg?raw=true)</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div><h2 id="超链接" tabindex="-1">超链接 <a class="header-anchor" href="#超链接" aria-hidden="true">#</a></h2><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight" tabindex="0"><code><span class="line"><span style="color:#A6ACCD;">[前往下载 ](https://github.com/KoolCore/ikoolcore/blob/main/docs/files/openwrt-x64-iKoolCore-R1-squashfs-combined-efi.img.gz)</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div><h2 id="标题区域-对vitepress没用" tabindex="-1">标题区域（对vitepress没用） <a class="header-anchor" href="#标题区域-对vitepress没用" aria-hidden="true">#</a></h2><p>自动生成侧边栏标题</p><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight" tabindex="0"><code><span class="line"><span style="color:#A6ACCD;">---</span></span>
+<span class="line"><span style="color:#A6ACCD;">sidebar: auto</span></span>
+<span class="line"><span style="color:#A6ACCD;">---</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div><p>禁用侧边栏标题</p><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight" tabindex="0"><code><span class="line"><span style="color:#A6ACCD;">---</span></span>
+<span class="line"><span style="color:#A6ACCD;">sidebar: false</span></span>
+<span class="line"><span style="color:#A6ACCD;">---</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div>`,20),p=[e];function o(c,i,r,C,d,A){return n(),a("div",null,p)}const y=s(t,[["render",o]]);export{h as __pageData,y as default};
